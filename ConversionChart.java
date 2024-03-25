c19e3dde7e08a75f1deb6a0014765a38dd06b530
akooy1118 <137736630+akooy1118@users.noreply.github.com>
2024-03-25 16:03:06 -0500

    Add files via upload

---
+++ ConversionChart.java
@@ -0,0 +1,23 @@
+public class ConversionChart {
+    
+    public static void main(String[] args) {
+        System.out.println("Feet      Meters");
+        for (double feet = 1.0; feet <= 10.0; feet++) {
+            System.out.printf("%-10.1f%-10.3f\n", feet, footToMeter(feet));
+        }
+        
+        System.out.println("\nMeters    Feet");
+        for (double meters = 20.0; meters <= 65.0; meters += 5.0) {
+            System.out.printf("%-10.1f%-10.3f\n", meters, meterToFoot(meters));
+        }
+    }
+    
+  
+    public static double footToMeter(double foot) {
+        return 0.305 * foot;
+    }
+
+    public static double meterToFoot(double meter) {
+        return 3.279 * meter;
+    }
+}
