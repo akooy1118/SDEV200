ea7fe878d74c3f5de1801da16eef78f63b5dae1b
akooy1118 <137736630+akooy1118@users.noreply.github.com>
2024-03-25 16:07:09 -0500

    Update ConversionChart.java

--- ConversionChart.java
+++ ConversionChart.java
@@ -1,5 +1,5 @@
 public class ConversionChart {
-    
+   /** prints charts */ 
     public static void main(String[] args) {
         System.out.println("Feet      Meters");
         for (double feet = 1.0; feet <= 10.0; feet++) {
@@ -12,11 +12,11 @@ public class ConversionChart {
         }
     }
     
-  
+    /** Conversion for feet to meters */
     public static double footToMeter(double foot) {
         return 0.305 * foot;
     }
-
+    /** Coversion for meters to feet */
     public static double meterToFoot(double meter) {
         return 3.279 * meter;
     }
