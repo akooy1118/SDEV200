1137f87d2a159b4f155c0050528955a40cf9448d
akooy1118 <137736630+akooy1118@users.noreply.github.com>
2024-04-21 22:56:36 -0500

    Create image.java

---
+++ image.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Hbox;
+import javafx.scene.layout.Pane;
+import javafx.geometry.Insets;
+import javafx.stage.Stage;
+import java.scene.image.Image;
+import java.scene.image.ImageView;
+
+public class Images extends Application {
+  @Override
+
+  public void start(Stage primaryStage) {
+    Pane pane = new Hbox(10);
+  }
+}
